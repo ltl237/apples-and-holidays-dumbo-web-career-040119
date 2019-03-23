c4e3a8f4784cd7dc06bf4b20790afc1e7d628c7b
Lorenzo Llamas <ltl924@gmail.com>
2019-03-23 21:04:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, values|
     puts "#{season.capitalize}:"
-    "#{values}: #{values.values}"
+    puts "#{values}: #{values.values.chomp}"
   end
 end
 
