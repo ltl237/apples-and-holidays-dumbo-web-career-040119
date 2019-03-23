073990ef5bd44bb2fecc6d7834a0f82b422c1cb1
Lorenzo Llamas <ltl924@gmail.com>
2019-03-23 21:03:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, values|
-    puts "#{season}:"
+    puts "#{season.capitalize}:"
     puts "#{values}: #{values.values}"
   end
 end
